8d90bcf4058319f09c0937ae780978a402e3bdab
Carlos Pires <extraterra1@gmail.com>
2026-01-03 12:32:05 +0000

    fix: app name and title

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'Mobile Shopping List',
+        name: 'Shopping List',
         short_name: 'ShopList',
         description: 'A minimalist mobile-first shopping list.',
         theme_color: '#ffffff',
